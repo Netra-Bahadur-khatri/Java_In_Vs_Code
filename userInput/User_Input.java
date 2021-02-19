f34ce68ff24c2131fad088fec7beb97349199c80
Netra Kc <71728112+Netra-Bahadur-khatri@users.noreply.github.com>
2021-02-19 16:23:01 +0545

    Add files via upload

---
+++ userInput/User_Input.java
@@ -0,0 +1,23 @@
+package userInput;
+
+import java.util.Scanner;
+
+public class User_Input {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+//		int x = sc.nextInt();
+		
+//		double y = sc.nextDouble();// for double value
+		
+		String hello = sc.nextLine();
+		
+//		System.out.println(x);
+//		System.out.println(y);
+		System.out.println(hello);
+	}
+
+}
